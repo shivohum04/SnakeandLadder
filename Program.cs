3809cb95980fc6b1835c229e8252c8159617d55d
shivohum04 <shivohum04@gmail.com>
2024-01-19 12:41:12 +0530

    [s] ADD: UC2 added

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+
+namespace UC1on7
+{
+    class Program
+    {
+        static void Roll()
+        {
+
+            Random random = new Random();
+            int value = random.Next(1, 6);
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+        static void Main()
+        {
+            int[] board = new int[100];
+            for (int i = 0; i < board.Length; i++)
+            {
+                Roll();
+            }
+        }
+    }
+}
